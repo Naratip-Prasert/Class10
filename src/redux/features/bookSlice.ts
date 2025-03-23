7ef90f9532ad5f80a0ea2dc3ff2e31eeb9a8b32f
Naratip-Prasert <6733121621@student.chula.ac.th>
2025-03-23 21:23:42 +0700

    TestCastF

--- src/redux/features/bookSlice.ts
+++ src/redux/features/bookSlice.ts
@@ -1,30 +1,37 @@
 import  { createSlice,PayloadAction} from "@reduxjs/toolkit"
 import { BookingItem } from "../../../interface"
 
-type BookState = {
-    bookItems : BookingItem[]
+type BookState={
+    bookItems:BookingItem[];
 }
 
-const initialState:BookState = {bookItems:[]}
+const initialState:BookState={bookItems:[]};
 
 export const bookSlice = createSlice({
-    name:"booking",
-    initialState,
-    reducers:{
-        addBooking: (state,action:PayloadAction<BookingItem>)=>{
-            state.bookItems.push(action.payload)
-        },
-        removeBooking:(state,action:PayloadAction<BookingItem>)=>{
-          const remainItems =  state.bookItems.filter( Object => {
-                return((Object.nameLastname !== action.payload.nameLastname)
-                        || (Object.tel !== action.payload.tel)
-                        || (Object.venue !== action.payload.venue)
-                        || (Object.bookDate !== action.payload.bookDate))
-            })
-            state.bookItems = remainItems
-        }
-    }
-})
-
-export const { addBooking,removeBooking } = bookSlice.actions
+  name: "book",
+  initialState,
+  reducers: {
+    addBooking: (state, action: PayloadAction<BookingItem>) => {
+      const index = state.bookItems.findIndex(
+        (item) => item.venue === action.payload.venue
+      );
+      if (index !== -1) {
+        
+        state.bookItems[index] = action.payload;
+      } else {
+       
+        state.bookItems.push(action.payload);
+      }
+    },
+    removeBooking: (state, action: PayloadAction<BookingItem>) => {
+        state.bookItems = state.bookItems.filter(
+          (item) =>
+            item.nameLastname !== action.payload.nameLastname ||
+            item.tel !== action.payload.tel ||
+            item.venue !== action.payload.venue ||
+            item.bookDate !== action.payload.bookDate
+        );
+    },
+}})
+        export const { addBooking,removeBooking } = bookSlice.actions
 export default bookSlice.reducer
